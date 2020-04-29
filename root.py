b27f849cecdd3fdb73921cdf7d48048eef1a663d
Theo Lemaire <theo.lemaire@epfl.ch>
2020-04-29 12:37:28 +0200

    corrected toffset in figure_09 notebook.

--- root.py
+++ root.py
@@ -3,6 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2020-04-28 14:04:01
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2020-04-28 22:25:38
+# @Last Modified time: 2020-04-29 12:36:38
 
 dataroot = None
+# dataroot = 'C:\\Users\\lemaire\\Documents\\SONIC paper data'
