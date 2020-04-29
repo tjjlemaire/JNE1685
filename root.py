26e06a818d5acba7f18e20028a30c27ed8b28538
Theo Lemaire <theo.lemaire@epfl.ch>
2020-04-29 14:44:51 +0200

    added mpi feature to actmap threshold curve in figure 7 notebook.

--- root.py
+++ root.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2020-04-28 14:04:01
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2020-04-29 13:32:31
+# @Last Modified time: 2020-04-29 14:43:30
 
 dataroot = '../../data/SONIC paper data/'
 # dataroot = 'C:\\Users\\lemaire\\Documents\\SONIC paper data'
