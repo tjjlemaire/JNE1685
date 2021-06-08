9855d9e3241734ff6d813752c095142f5dd76766
Theo Lemaire <theo.lemaire@epfl.ch>
2021-06-08 14:50:42 +0200

    changed data root

--- root.py
+++ root.py
@@ -3,14 +3,14 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2020-04-28 14:04:01
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2020-04-29 15:09:06
+# @Last Modified time: 2021-06-08 14:50:04
 
 import platform
 
 os_name = platform.system()
 if os_name == 'Windows':
-    dataroot = 'C:\\Users\\lemaire\\Documents\\SONIC paper data'
+    dataroot = 'C:\\Users\\lemaire\\Documents\\papers data\\SONIC'
 elif os_name == 'Linux':
-    dataroot = '../../data/SONIC paper data/'
+    dataroot = '../../data/SONIC/'
 else:
     dataroot = None
